754a61eb8c19edfc75ad43671d5734bb6e517e21
minchina <minchina91@gmail.com>
2014-11-02 16:36:31 +0800

    update:no barkets,push unit and symbol to stack

--- test/src/Filter.js
+++ test/src/Filter.js
@@ -6,13 +6,39 @@ function RulerFilter(){
 
 RulerFilter.filter=function(goods_items,rule){
     rule = RulerFilter.remove_no_use_symbal(rule);
-    return RulerFilter.filter_process(goods_items,rule);
+    return RulerFilter.filter_process(goods_items,rule,[],[]);
 
 };
 
-RulerFilter.filter_process=function(good_items,rule){
-    var unit = RulerFilter.getValueMap(rule);
-    return RulerFilter.get_good_by_unit_rule(good_items,unit);
+RulerFilter.filter_process=function(good_items,rule,symbol_stack,result_stack){
+    //规则中包含有|或者&
+    if(rule.indexOf("|") !=-1 || rule.indexOf("&")!=-1){
+        //并且不是以|&开头
+        if(rule.match(/[|&]/).index !=0){
+            var index = rule.match(/[|&]/);
+            var unit = rule.slice(0,index.index);
+            var map = RulerFilter.getValueMap(unit);
+            result_stack.push(RulerFilter.get_good_by_unit_rule(good_items,map));
+            rule = rule.slice(index.index);
+            return RulerFilter.filter_process(good_items,rule,symbol_stack,result_stack);
+            //以|&开头，肯定不是第一次进入该函数
+        }else if(rule.match(/[|&]/).index ==0){
+            //将该符号压入结果栈
+            symbol_stack.push(rule.slice(0,1));
+            rule = rule.slice(1);
+            return RulerFilter.filter_process(good_items,rule,symbol_stack,result_stack);
+        }
+    }
+    //当规则中不包含|&的时候
+    if(rule.indexOf("|") ==-1 || rule.indexOf("&")==-1){
+        console.log(rule,symbol_stack,result_stack);
+        //符号栈中有符号，说明不是单个unit
+        if(symbol_stack.length!=0){
+
+        }else{//单个单元
+
+        }
+    }
 };
 
 RulerFilter.remove_no_use_symbal = function(rule){
@@ -41,6 +67,10 @@ RulerFilter.get_good_by_unit_rule=function(gooditems,unit){
             return item[key] > value;
         }
     });
+};
+
+RulerFilter.and_compute = function(){
+
 };
 
 function icp(symbol) {

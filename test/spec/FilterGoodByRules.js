0acc2209909f9f038018e55d4e574313aed86b0f
minchina <minchina91@gmail.com>
2014-11-17 19:57:01 +0800

    add:filter good by rule js

--- test/spec/FilterGoodByRules.js
+++ test/spec/FilterGoodByRules.js
@@ -1,3 +1,6 @@
-/**
- * Created by cuitmnc on 2014/11/17.
- */
+describe('filter correct goods by rule which have bracket', function () {
+    it("filter  苹果 by (name=='苹果') ",function(){
+        var rule = "(name=='苹果')";
+        var result = RulerFilter.filter()
+    });
+});

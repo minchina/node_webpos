2292a61802cab28720da3a4692a4c75eaace4dd7
minchina <minchina91@gmail.com>
2014-11-17 19:43:40 +0800

    add:parse function tested by brackets has brackets

--- test/spec/FunctionSpec.js
+++ test/spec/FunctionSpec.js
@@ -1,9 +1,17 @@
 describe('some small model should work well', function () {
 
+
     it("parse function should instead Brackets by $",function(){
         var rule = "(name=='苹果')";
+        rule = RulerFilter.remove_no_use_symbal(rule);
         var result = RulerFilter.parse(rule,[]);
         expect(result.rule).toBe("$0");
-        expect(result.list[0]).toBe("name=='苹果'");
+        expect(result.list[0]).toBe("name=苹果");
+    });
+    it("parse function should work well in brackets have brackets",function(){
+        var rule = "(name=='苹果') || ((type=='电子产品' || name =='可比克') && date<2014/10/10)";
+        rule = RulerFilter.remove_no_use_symbal(rule);
+        var result = RulerFilter.parse(rule,[]);
+        expect(result.rule).toBe("$2|$1");
     })
 });
